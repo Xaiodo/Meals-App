f48689cc122e9bc92245a67a6ce7e30c91e86ce9
Xaiodo <rostislavgovbak@gmail.com>
2023-02-17 19:49:18 +0200

    project set up

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+import './dummy_data.dart';
+import './models/meal.dart';
+import './screens/filters_screen.dart';
+import './screens/meal_detail_screen.dart';
+import './screens/tab_screen.dart';
+import './screens/categories_screen.dart';
+import './screens/category_meals_screen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
+
+  List<Meal> _availableMeals = dummyMeals;
+  List<Meal> _favoriteMeals = [];
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _availableMeals = dummyMeals.where((element) {
+        if (_filters['gluten'] == true && !element.isGlutenFree) {
+          return false;
+        }
+        if (_filters['lactose'] == true && !element.isLactoseFree) {
+          return false;
+        }
+        if (_filters['vegan'] == true && !element.isVegan) {
+          return false;
+        }
+        if (_filters['vegetarian'] == true && !element.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
+  bool isMealFavorite(String id) {
+    return _favoriteMeals.any((element) => element.id == id);
+  }
+
+  void _toggleFavorites(String mealId) {
+    final existingIndex =
+        _favoriteMeals.indexWhere((element) => element.id == mealId);
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteMeals
+            .add(dummyMeals.firstWhere((element) => element.id == mealId));
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blueGrey,
+          accentColor: const Color.fromRGBO(255, 17, 130, 1),
+        ),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyMedium: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyLarge: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              titleLarge: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Raleway',
+              ),
+            ),
+      ),
+      routes: {
+        '/': (context) => TabsScreen(favoriteMeals: _favoriteMeals),
+        CategoryMealsScreen.routeName: (context) =>
+            CategoryMealsScreen(availableMeals: _availableMeals),
+        MealDetailScreen.routeName: (context) => MealDetailScreen(
+            toggleFavorites: _toggleFavorites, isMealFavorite: isMealFavorite),
+        FiltersScreen.routeName: (context) =>
+            FiltersScreen(currentFilters: _filters, saveFilters: _setFilters),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (context) => const CategoriesScreen(),
+        );
+      },
+    );
+  }
+}
